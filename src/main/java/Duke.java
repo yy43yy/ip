cbb03675a527a073178476373ece0359b291f814
yy43yy <yiyang15624@gmail.com>
2026-02-08 18:49:10 +0800

    Update variable name for coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,60 +4,60 @@ public class Duke {
 
 
     public static void main(String[] args) {
-        String line;
+        String input;
         Scanner in = new Scanner(System.in);
 
-        Task[] addList = new Task[100];
-        int i = 0;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm yy");
         System.out.println("What can I do for you?");
 
         while (true) {
-            line = in.nextLine();
+            input = in.nextLine();
 
-            if (line.contains("list")) {
-                for (int j = 0; j < i; j++) {
-                    System.out.println((j+1) + ". " + addList[j]);
+            if (input.contains("list")) {
+                for (int j = 0; j < taskCount; j++) {
+                    System.out.println((j+1) + ". " + tasks[j]);
                 }
-            } else if (line.contains("blah")) {
+            } else if (input.contains("blah")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("blah");
                 System.out.println("____________________________________________________________");
 
-            } else if (line.contains("bye")) {
+            } else if (input.contains("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else if(line.startsWith("mark ")){
-                String[] parts = line.split(" ");
-                int numb = Integer.parseInt(parts[1]);
-                addList[numb-1].markDone();
+            } else if(input.startsWith("mark ")){
+                String[] parts = input.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]);
+                tasks[taskNumber-1].markDone();
 
 
                 System.out.println("____________________________________________________________");
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(addList[numb-1]);
+                System.out.println(tasks[taskNumber-1]);
                 System.out.println("____________________________________________________________");
 
 
-            } else if(line.startsWith("unmark ")){
-                String[] parts = line.split(" ");
-                int numb = Integer.parseInt(parts[1]);
-                addList[numb-1].unmarkDone();
+            } else if(input.startsWith("unmark ")){
+                String[] parts = input.split(" ");
+                int taskNumberblah = Integer.parseInt(parts[1]);
+                tasks[taskNumber-1].unmarkDone();
 
                 System.out.println("____________________________________________________________");
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(addList[numb-1]);
+                System.out.println(tasks[taskNumber-1]);
                 System.out.println("____________________________________________________________");
 
 
             }else {
-                addList[i] = new Task(line);
-                i++;
+                tasks[taskCount] = new Task(input);
+                taskCount++;
                 System.out.println("____________________________________________________________");
-                System.out.println(" added: " + line);
+                System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
 
             }
