b4e488a019499da79db49c93c979c3275982d36a
yy43yy <yiyang15624@gmail.com>
2026-02-10 14:25:44 +0800

    Improve Code Quality
    use switch instead of if else if, added function for cleaner view

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,74 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void printLine(){
+        System.out.println("____________________________________________________________");
+    }
 
+    public static void printList(Task[] tasks, int taskCount){
+        for(int i=0;i<taskCount;i++){
+            System.out.println((i+1)+". "+tasks[i]);
+        }
+    }
+
+    public static void handleMark(String input,Task[] tasks){
+        String[] parts = input.split(" ");
+        int taskNumber = Integer.parseInt(parts[1]);
+        tasks[taskNumber-1].markDone();
+
+        printLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(tasks[taskNumber-1]);
+        printLine();
+    }
+
+    public static void handleUnmark(String input,Task[] tasks){
+        String[] parts = input.split(" ");
+        int taskNumber = Integer.parseInt(parts[1]);
+        tasks[taskNumber-1].unmarkDone();
+
+        printLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(tasks[taskNumber-1]);
+        printLine();
+    }
+
+    public static int handleTodo(String input,Task[] tasks,int taskCount ){
+        input = input.substring(4).trim();
+        tasks[taskCount] = new Todos(input);
+
+        printLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[taskCount].toString());
+        taskCount++;
+        System.out.println("Now you have " + taskCount + " tasks in the list");
+        printLine();
+        return taskCount;
+    }
+    public static int handleDeadline(String input,Task[] tasks,int taskCount){
+        input = input.substring((8)).trim();
+        String[] parts = input.split("/by");
+        tasks[taskCount] = new Deadlines(parts[0],parts[1]);
+        printLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[taskCount].toString());
+        taskCount++;
+        System.out.println("Now you have " + taskCount + " tasks in the list");
+        printLine();
+        return taskCount;
+    }
+    public static int handleEvent(String input,Task[] tasks,int taskCount){
+        input = input.substring((5)).trim();
+        String[] parts = input.split("/");
+        tasks[taskCount]= new Events(parts[0],parts[1].substring(4),parts[2].substring(2));
+        printLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[taskCount].toString());
+        taskCount++;
+        System.out.println("Now you have " + taskCount + " tasks in the list");
+        printLine();
+        return taskCount;
+    }
 
     public static void main(String[] args) {
         String input;
@@ -9,90 +76,41 @@ public class Duke {
 
         Task[] tasks = new Task[100];
         int taskCount = 0;
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println("Hello! I'm yy");
         System.out.println("What can I do for you?");
 
         while (true) {
             input = in.nextLine();
+            String [] words = input.split(" ",2);
+            String command = words[0];
 
-            if (input.contains("list")) {
-                for (int j = 0; j < taskCount; j++) {
-                    System.out.println((j+1) + ". " + tasks[j]);
-                }
-            } else if (input.contains("blah")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("blah");
-                System.out.println("____________________________________________________________");
-
-            } else if (input.contains("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
-            } else if(input.startsWith("mark ")){
-                String[] parts = input.split(" ");
-                int taskNumber = Integer.parseInt(parts[1]);
-                tasks[taskNumber-1].markDone();
-
-
-                System.out.println("____________________________________________________________");
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(tasks[taskNumber-1]);
-                System.out.println("____________________________________________________________");
-
-
-            } else if(input.startsWith("unmark ")){
-                String[] parts = input.split(" ");
-                int taskNumber = Integer.parseInt(parts[1]);
-                tasks[taskNumber-1].unmarkDone();
-
-                System.out.println("____________________________________________________________");
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(tasks[taskNumber-1]);
-                System.out.println("____________________________________________________________");
-
-
-            } else if(input.startsWith("todo")){
-                input = input.substring(4).trim();
-                tasks[taskCount] = new Todos(input);
-
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[taskCount].toString());
-                taskCount++;
-                System.out.println("Now you have " + taskCount + " tasks in the list");
-                System.out.println("____________________________________________________________");
-
-            } else if(input.startsWith("deadline")){
-                input = input.substring((8)).trim();
-                String[] parts = input.split("/by");
-                tasks[taskCount] = new Deadlines(parts[0],parts[1]);
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[taskCount].toString());
-                taskCount++;
-                System.out.println("Now you have " + taskCount + " tasks in the list");
-                System.out.println("____________________________________________________________");
-
-            }else if(input.startsWith("event")){
-                input = input.substring((5)).trim();
-                String[] parts = input.split("/");
-                tasks[taskCount]= new Events(parts[0],parts[1].substring(4),parts[2].substring(2));
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println(tasks[taskCount].toString());
-                taskCount++;
-                System.out.println("Now you have " + taskCount + " tasks in the list");
-                System.out.println("____________________________________________________________");
-
-            }else {
-                tasks[taskCount] = new Task(input);
-                taskCount++;
-                System.out.println("____________________________________________________________");
-                System.out.println(" added: " + input);
-                System.out.println("____________________________________________________________");
-
+            switch (command){
+                case"list":
+                    printList(tasks,taskCount);
+                    break;
+                case "mark":
+                    handleMark(input,tasks);
+                    break;
+                case "unmark":
+                    handleUnmark(input,tasks);
+                    break;
+                case "todo":
+                    taskCount=handleTodo(input,tasks,taskCount);
+                    break;
+                case "deadline":
+                    taskCount=handleDeadline(input,tasks,taskCount);
+                    break;
+                case "event":
+                    taskCount=handleEvent(input,tasks,taskCount);
+                    break;
+                case "bye":
+                    printLine();
+                    System.out.println(" Bye. Hope to see you again soon!");
+                    printLine();
+                    return;
+                default:
+                    System.out.println("Invalid command, type something else");
             }
 
         }
